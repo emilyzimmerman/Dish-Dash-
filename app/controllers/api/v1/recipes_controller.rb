ee0b54ee98de997dda55b98afd544cb71a58e9a7
emilyzimmerman <zimmermanemily121@gmail.com>
2023-03-22 22:22:24 -0500

    Added Other CRUD operations for Recipes
    Still need to fix meals

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -10,6 +10,44 @@ module Api
                 }
                 render_success(payload: payload)
             end
+
+            def index 
+                recipes = Recipe.all 
+
+                payload = {
+                    recipe: RecipeBlueprint.render_as_hash(recipes),
+                    status: 200
+                }
+                render_success(payload: payload)
+            end
+
+            def show 
+                recipe = Recipe.find(params[:id])
+
+                payload = {
+                    recipe: RecipeBlueprint.render_as_hash(recipe),
+                    status: 200
+                }
+                render_success(payload: payload)
+            end
+
+            def update 
+                result = Recipes::Operations.update_recipe(params, @current_user)
+                render_error(errors: result.errors.all, status: 400) and return unless result.success?
+                payload = {
+                    recipe: RecipeBlueprint.render_as_hash(result.payload), 
+                    status: 201
+                }
+                render_success(payload: payload)
+            end
+
+            def destroy
+                #exception --> refer to "rescue operation"
+                recipe = Recipe.find(params[:id])
+
+                recipe.destroy 
+                render_success(payload: "Recipe successfully destroyed", status: 200)
+            end
         end
     end
 end
--- app/services/recipes/operations.rb
+++ app/services/recipes/operations.rb
@@ -1,9 +1,18 @@
 module Recipes 
     module Operations
         def self.new_recipe(params, current_user)
+            #Define recipe
             recipe = current_user.recipes.new(name: params[:name], content: params[:content], image_path: params[:image_path], meal_id: params[:meal_id])
 
             return ServiceContract.success(recipe) if recipe.save
+
+            ServiceContract.error(recipe.errors.full_messages)
+        end
+
+        def self.update_recipe(params, current_user)
+            # Grab existing recipe
+            recipe = Recipe.find(params[:id])
+            return ServiceContract.success(recipe) if recipe.update(name: params[:name], content: params[:content], image_path: params[:image_path], meal_id: params[:meal_id])
 
             ServiceContract.error(recipe.errors.full_messages)
         end
