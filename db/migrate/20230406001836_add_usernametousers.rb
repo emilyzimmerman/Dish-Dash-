cd56a13630a93b3833b32d58a46d56a2c1ea5b45
emilyzimmerman <zimmermanemily121@gmail.com>
2023-04-06 15:27:19 -0500

    Added fake data to seed

---
+++ db/migrate/20230406001836_add_usernametousers.rb
@@ -0,0 +1,5 @@
+class AddUsernametousers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Users
+(0..10).each do 
+    User.create(
+        email: Faker::Internet.email, 
+        first_name: Faker::TvShows::BigBangTheory.character, 
+        last_name: Faker::Name.last_name, 
+        phone: "1", 
+        password: "password", 
+        username: Faker::Fantasy::Tolkien.character)
+end
+
+#Meals
+Meal.create(mealType: "Breakfast")
+Meal.create(mealType: "Lunch")
+Meal.create(mealType: "Dinner")
+Meal.create(mealType: "Dessert")
+Meal.create(mealType: "Snacks")
+
+#Recipes
+# - meal
+# - user 
+# - recipe content
+(0..100).each do |recipe|
+    faker_name = Faker::Food.dish 
+    faker_content = Faker::Food.description
+
+    recipe = Recipe.new(user_id: rand(0..9), name: faker_name, content: faker_content, 
+        image_path: "https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg")
+        recipe.meal = Meal.all.sample
+        recipe.ingredients = Ingredient.all.sample(rand(0..100))
+
+        recipe.save
+end
+
+#ingredients
+(0..100).each do |ingredient|
+    faker_ingredient = Faker::Food.ingredient 
+    faker_quantity = Faker::Number.number(digits: 3)
+
+    ingredient = Ingredient.new(user_id: rand(0..9), quantity: faker_quantity, name: faker_ingredient)
+    ingredient.save
+end
